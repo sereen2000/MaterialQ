a59156dab7ce7f440333d1ceb0234562575a0149
sereen2000 <127016744+sereen2000@users.noreply.github.com>
2025-11-08 14:09:16 +0300

    my project

---
+++ MaterialQ/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialQ.Controllers
+{
+    [Authorize]
+    public class MainController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
